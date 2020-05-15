dde6d5bffc4da2222cf68f1d426203b0fbc53b0b
Tomasz Jablonski <jabolovp@gmail.com>
2020-05-15 20:21:33 +0200

    Dodanie modelu user, typedefs i resolwera

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,11 @@ import Sequelize from "sequelize";
 import db from "../configs/database";
 
 const User = db.define("users", {
-  name: Sequelize.TEXT,
+  role: Sequelize.STRING,
+  email: Sequelize.STRING,
   password: Sequelize.STRING,
+  first_name: Sequelize.STRING,
+  last_name: Sequelize.STRING,
 });
 
 export default User;
