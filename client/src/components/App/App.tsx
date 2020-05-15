2f1444562d03622ae6738e49d68d805eda5c59cf
Dawid Grzelecki <davidgrzelecki@gmail.com>
2020-05-15 17:41:20 +0200

    Initial commit

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import './App.sass';
+
+const App: FunctionComponent = () => {
+	return (
+		<div className="App">
+			
+		</div>
+	)
+}
+
+export default App;
