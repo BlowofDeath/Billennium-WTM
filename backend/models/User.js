19dd3f1e00d30d4bda92af47a151470ba98aadd9
Tomasz Jablonski <jabolovp@gmail.com>
2020-05-21 14:00:55 +0200

    Usunięto tabele role, dodano pole role do usera

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,15 @@
 import Sequelize from "sequelize";
 import db from "../configs/database";
 import Month from "./Month";
-import Role from "./Role";
 
 const User = db.define("users", {
   email: Sequelize.STRING,
   password: Sequelize.STRING,
   first_name: Sequelize.STRING,
   last_name: Sequelize.STRING,
+  role: Sequelize.STRING,
 });
 
 User.hasMany(Month);
-User.hasOne(Role);
 
 export default User;
