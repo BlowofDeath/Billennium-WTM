2905a80fcaefcbd6869a52d565dd8c2601a15aaa
Dawid Grzelecki <davidgrzelecki@gmail.com>
2020-05-29 15:27:55 +0200

    Settlements page frame

--- client/src/components/ManagerSettlementsPage/ManagerSettlementsPage.tsx
+++ client/src/components/ManagerSettlementsPage/ManagerSettlementsPage.tsx
@@ -7,16 +7,24 @@ import { Context } from '../App/Context';
 import SettlementsList from '../SettlementsList/SettlementsList';
 import Loader from '../Loader/Loader';
 import Panel from '../vendor/Panel/Panel';
+import { useLocation } from 'react-router-dom';
+import { Row } from '../Atoms/Row';
 
 const ManagerSettlementPage: FC = ({}) => {
-	const month = moment().month() + 1;
-	const year = moment().year();
+	const { search } = useLocation();
+	const query = new URLSearchParams(search);
+	const now = moment();
+	const year = query.get('year') ? parseInt(query.get('year') as string) : now.year();
+	const month = query.get('month') ? parseInt(query.get('month') as string) : now.month() + 1;
+
 	const { token } = useContext(Context);
 	const { data, loading, error } = useQuery(ManagerSettlementsQuery, {
 		variables: {
 			year, month, token
 		}
 	});
+
+	console.log({ data });
 	
 	if (loading)
 		return <Loader loading={true}/>;
@@ -29,7 +37,17 @@ const ManagerSettlementPage: FC = ({}) => {
 		<Page>
 			<h2>Rozliczenia</h2>
 			<Panel>
+				<Row>
+					<h3>Miesiąc - {month}</h3>
+					<span>{ data?.monthForAllUsers.isClosed ? "ZAMKNIĘTY" : "OTWARTY" }</span>
+				</Row>
+			</Panel>
+			<Panel>
+				<h3>Oczekujące</h3>
 				<SettlementsList settlements={data.monthForAllUsers}/>
+			</Panel>
+			<Panel>
+				<h3>Zaległe</h3>
 			</Panel>
 		</Page>
 	)
