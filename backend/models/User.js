2dca7354dee12853070e0867f3cb39067ee7bc29
Dawid Grzelecki <davidgrzelecki@gmail.com>
2020-06-01 22:09:36 +0200

    merge backend into devel

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+import Sequelize from "sequelize";
+import db from "../configs/database";
+import Month from "./Month";
+import WorkTimeRecord from "./WorkTimeRecord";
+
+const User = db.define("users", {
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  first_name: Sequelize.STRING,
+  last_name: Sequelize.STRING,
+  role: {
+    type: Sequelize.ENUM("Pracownik", "Kierownik", "Admin"),
+    defaultValue: "Pracownik",
+  },
+  salary: Sequelize.INTEGER,
+  isActive: Sequelize.BOOLEAN,
+});
+
+User.hasMany(Month);
+Month.belongsTo(User);
+
+export default User;
